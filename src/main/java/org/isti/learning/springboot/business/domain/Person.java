2e35e1371572c54d62eca3a413b8456c6bb88e42
Rejtő István <reistvan@gmail.com>
2020-03-07 16:59:06 +0100

    Add missed changes for API to return with list of PersonDto

--- src/main/java/org/isti/learning/springboot/business/domain/Person.java
+++ src/main/java/org/isti/learning/springboot/business/domain/Person.java
@@ -1,7 +1,16 @@
 package org.isti.learning.springboot.business.domain;
 
 public class Person {
+    private int id;
     private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
--- src/main/java/org/isti/learning/springboot/business/interfaces/Database.java
+++ src/main/java/org/isti/learning/springboot/business/interfaces/Database.java
@@ -1,8 +1,12 @@
 package org.isti.learning.springboot.business.interfaces;
 
+import org.isti.learning.springboot.business.domain.Person;
+
+import java.util.List;
+
 public interface Database {
     String insertData(int id, String name);
-    String getData();
+    List<Person> getData();
     String createTable();
     String dropTable();
 }
