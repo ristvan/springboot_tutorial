033dfdb42f10ca81ce41430e1428f917f74c53f0
Rejtő István <reistvan@gmail.com>
2020-03-09 22:03:53 +0100

    Add two more test cases
    Signed-off-by: Rejtő István <reistvan@gmail.com>

--- src/test/java/org/isti/learning/springboot/end2end/HelloControllerTest.java
+++ src/test/java/org/isti/learning/springboot/end2end/HelloControllerTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.hamcrest.Matchers.equalTo;
@@ -19,12 +20,42 @@ public class HelloControllerTest {
     private MockMvc mvc;
 
     @Test
-    public void testEmptyDb_getReturnWithEmptyList() throws Exception {
+    public void testEmptyDb_getReturnsWithEmptyList() throws Exception {
         mvc.perform(MockMvcRequestBuilders.post("/db-create").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
         mvc.perform(MockMvcRequestBuilders.get("/db-get").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().string(equalTo("{\"numberOfPeople\":0,\"people\":[]}")));
+        mvc.perform(MockMvcRequestBuilders.delete("/db-drop").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testAddingOneName_getReturnsWithListThatContainsOnePerson() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.post("/db-create").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        mvc.perform(MockMvcRequestBuilders.post("/db-insert?id=1&name=Detti").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        String expectedAnswer = "{\"numberOfPeople\":1,\"people\":[{\"id\":1,\"name\":\"Detti\"}]}";
+        mvc.perform(MockMvcRequestBuilders.get("/db-get").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo(expectedAnswer)));
+        mvc.perform(MockMvcRequestBuilders.delete("/db-drop").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testAddingTwoNames_getReturnsWithListThatContainsTwoPeople() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.post("/db-create").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        mvc.perform(MockMvcRequestBuilders.post("/db-insert?id=1&name=Detti").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        mvc.perform(MockMvcRequestBuilders.post("/db-insert?id=2&name=Isti").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        String expectedAnswer = "{\"numberOfPeople\":2,\"people\":[{\"id\":1,\"name\":\"Detti\"},{\"id\":2,\"name\":\"Isti\"}]}";
+        mvc.perform(MockMvcRequestBuilders.get("/db-get").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo(expectedAnswer)));
         mvc.perform(MockMvcRequestBuilders.delete("/db-drop").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
     }
